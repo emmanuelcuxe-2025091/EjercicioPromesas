61c313c0aa457232f968cf5d4cbcecfabb67f837
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-07-13 20:43:08 -0600

    feat: añade dos usuarios más al arreglo

--- src/data/Usuario.ts
+++ src/data/Usuario.ts
@@ -13,4 +13,16 @@ export const usuarios: Usuario[] = [
         contrasena: "1234wdsa",
         correo: "jcuxe-2025091@kinal.edu.gt",
     },
+    {
+        id: 3,
+        nombre: "Carlos",
+        contrasena: "qwer4321",
+        correo: "carlosy@gmail.com",
+    },
+    {
+        id: 4,
+        nombre: "Diego",
+        contrasena: "zxcv0987",
+        correo: "diegoh@gmail.com",
+    },
 ];
