c6dc6c6d5d2187bea30478633aa8e1b7ebcbb4a0
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-07-13 19:08:37 -0600

    feat: agrega arreglo de Usuario de datos

---
+++ src/data/Usuario.ts
@@ -0,0 +1,16 @@
+import { Usuario } from '../models/usuario';
+
+export const usuarios: Usuario[] = [
+    {
+        id: 1,
+        nombre: "Emmanuel",
+        contrasena: "123asdw",
+        correo: "ecuxe-2025091@kinal.edu.gt",
+    },
+    {
+        id: 2,
+        nombre: "Josué",
+        contrasena: "1234wdsa",
+        correo: "jcuxe-2025091@kinal.edu.gt",
+    },
+];
